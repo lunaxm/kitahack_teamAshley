4e213bb7068e95a78b70005e07856d60eaad8855
lunaxm <125177814+lunaxm@users.noreply.github.com>
2026-02-28 00:11:59 +0800

    main.py reset

--- backend/main.py
+++ backend/main.py
@@ -61,11 +61,4 @@ app = FastAPI(title="kitahack_teamAshley Gateway", lifespan=lifespan)
 app.include_router(runpod_api_router, prefix = "/runpod")
 app.include_router(database_api_router, prefix = "/database")
 app.include_router(medgemma_api_router, prefix = "/medgemma")
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
